f0fd2468c7729d0c39cb6b080fdb8df952db40c3
Virtus Brædder Opstrup <virtusbradder@gmail.com>
2018-03-16 08:41:49 +0100

    h

---
+++ TEC_H2_Dating/ViewProfileInterests.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TEC_H2_Dating
+{
+    /// <summary>
+    /// Interaction logic for ViewProfileInterests.xaml
+    /// </summary>
+    public partial class ViewProfileInterests : Window
+    {
+        public ViewProfileInterests()
+        {
+            InitializeComponent();
+        }
+
+        private void VPIwindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
